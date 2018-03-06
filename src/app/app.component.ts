faf92f6929398761ab06ac2a533424457265989c
Mo Amao <moamao78@gmail.com>
2018-03-06 13:35:24 -0600

    initial upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { products } from './products';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  public gridData: any[] = products;
 }
